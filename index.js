d6df81eff717e22b48c93096d31f40b2a9647368
Kilian Koeltzsch <me@kilian.io>
2016-08-23 16:56:59 +0200

    add and display transport icons and time subtitle

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 #! /usr/local/bin/node
 
-var dvb = require('dvbjs');
+var dvb = require('dvbjs')
+var moment = require('moment')
+moment.locale('de')
 
-var args = process.argv.slice(2);
+var args = process.argv.slice(2)
 
-var timeOffset = 0;
-var numResults = 6;
+var timeOffset
+args[1] ? timeOffset = args[1] : timeOffset = 0
+
+var numResults = 6
 dvb.monitor(args[0], timeOffset, numResults)
     .then(function(data) {
-        var items = {'items': []};
+        var items = {'items': []}
 
         if (data.length === 0) {
             items.items.push({
                 'title': 'No such stop found 🤔'
-            });
-            console.log(JSON.stringify(items));
-            return;
+            })
+            console.log(JSON.stringify(items))
+            return
         }
 
         data.forEach(function(con) {
             items.items.push({
                 'title': con.line + ' ' + con.direction + ' in ' + con.arrivalTimeRelative + ' minutes',
-                // 'subtitle': con.arrivalTime,
-            });
-        });
+                'subtitle': moment(con.arrivalTime).format('dddd, HH:mm [Uhr]'),
+                'icon': {
+                    'path': 'transport_icons/' + con.mode.name + '.png'
+                }
+            })
+        })
 
-        console.log(JSON.stringify(items));
+        console.log(JSON.stringify(items))
     })
     .catch(function (err) {
         var items = {'items': [{
             'title': 'Ran into an error 😭',
             'subtitle': err,
-        }]};
-        console.log(items);
-        throw err;
-    });
+        }]}
+        console.log(items)
+        throw err
+    })
